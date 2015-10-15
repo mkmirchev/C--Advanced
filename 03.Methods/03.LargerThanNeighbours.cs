22ad742e30eaac37f4ac338355345db1ca00935a
Mircho Mirchev <mkmirchev@abv.bg>
2015-10-15 15:52:19 +0300

    Create 03.LargerThanNeighbours.cs

---
+++ 03.Methods/03.LargerThanNeighbours.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+
+class LargerThanNeighbours
+{
+    static void Main()
+    {
+        int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        for (int i = 0; i < input.Length; i++)
+        {
+            Console.WriteLine(IsLargerThanNeighbours(input, i));
+        }
+    }
+    public static bool IsLargerThanNeighbours(int[] numbers, int currnetNumber)
+    {
+        bool isLarger = false;
+        if (currnetNumber > 0 && currnetNumber < numbers.Length - 1)
+        {
+            if (numbers[currnetNumber] > numbers[currnetNumber - 1] && numbers[currnetNumber] > numbers[currnetNumber + 1])
+            {
+                isLarger = true;
+            }
+        }
+        else if (currnetNumber == 0)
+        {
+            if (numbers[currnetNumber] > numbers[currnetNumber + 1])
+            {
+                isLarger = true;
+            }
+        }
+        else if (currnetNumber == numbers.Length - 1)
+        {
+            if (numbers[currnetNumber] > numbers[currnetNumber - 2])
+            {
+                isLarger = true;
+            }
+        }
+        return isLarger;
+    }
+}
+
+
