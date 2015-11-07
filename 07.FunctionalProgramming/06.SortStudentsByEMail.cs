913f04f8ac8eb737581a42a98d389b348d1097a5
Mircho Mirchev <mkmirchev@abv.bg>
2015-11-07 11:41:09 +0200

    Create 06.SortStudentsByEMail.cs

---
+++ 07.FunctionalProgramming/06.SortStudentsByEMail.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentDataBase;
+
+namespace SortStudentsByEMail
+{    
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDirectory dB = new StudentDirectory();
+            string pattern = @"(?<=\w\@)abv\.bg?()";
+            Regex rgx = new Regex(pattern);
+            var abv = dB.Students.Where(email => rgx.IsMatch(email.Email));
+            var abvEmails = from student in dB.Students
+                            where rgx.IsMatch(student.Email)
+                            select student;
+
+            Console.WriteLine("LINQ");
+            foreach (var student in abv)
+            {                
+                Console.WriteLine("{0} {1} {2}",student.FirstName, student.LastName, student.Email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lambda");
+            foreach (var student in abvEmails)
+            {                               
+                Console.WriteLine("{0} {1} {2}", student.FirstName, student.LastName, student.Email);
+            }
+        }
+    }
+}
